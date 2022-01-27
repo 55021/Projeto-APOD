e36db5d742da850a27f5a553f48189827c704701
Maria Rita <msantana@id.uff.br>
2022-01-27 19:43:19 -0300

    starting js

---
+++ index.js
@@ -0,0 +1,21 @@
+/* SWITCH DO INPUT:
+display: block no title subtitle description
+display: none no input-title e input
+OUUUUU a ideia do edu: insert e remove class .invisible
+muda as cores da nav
+Tentar inserir slideinright na imagem
+
+API Key: j3gAcBzYdK0oGQdLD6Eaj9SF9Dq6NTIplK9ybvZi
+*/
+
+$(".apodbtn").click(function(date) {
+    /* var date1 = $("#apod-date").val().split("-");
+    var date = `${date1[2]}/${date1[1]}/${date1[0]}` */
+    var date = $("#apod-date").val();
+    console.log(date);
+    // setApod();
+})
+
+/* function setApod() {
+    $.ajax(, success)
+} */
