e55c3e146a3a0c5e2d0eb035496f85808f266c22
Maria Rita <msantana@id.uff.br>
2022-01-28 12:25:37 -0300

    added input placeholder. sort of 🌌

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+window.onload = function () {
+    let data = new Date();
+    let dia = data.getDate();
+    let mes = data.getMonth() +1;
+    let ano = data.getFullYear();
+
+    if (mes <= 9) {
+        mes = `0${mes}`;
+    }
+
+    if (dia <= 9) {
+        dia = `0${dia}`;
+    }
+
+    let calendario = `${ano}-${mes}-${dia}`;
+    $('#apod-date').val(calendario);
+}
+
 $(".apodbtn").click(function(date) {
     setApod();
 });
