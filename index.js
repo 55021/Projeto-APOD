81d6a0816afadbcecfb162bb83afc622f0fb7e6f
Maria Rita <msantana@id.uff.br>
2022-01-28 00:33:58 -0300

    refresh fix

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 $(".apodbtn").click(function(date) {
     setApod();
-})
+});
 
-$(".new-date").click(function(default) {
-    turnBack();
-})
-
-function turnBack() {
-    $('.input').removeClass('invisible');
-    $('.new-date').addClass('invisible');
-    $('.description').addClass('invisible');
-    $('.identification').addClass('invisible');
-    $('.copyright').addClass('invisible');
-    $('.pic').addClass('invisible');
-}
+$(".new-date").click(function() {
+    location.reload();
+});
 
 function setApod() {    
     var date = $("#apod-date").val();
